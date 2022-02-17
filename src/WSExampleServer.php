d55375aedc9b698157fd32f77d7ea7c1ebb80296
Rekryt <rekrytkw@gmail.com>
2022-02-17 16:14:27 +0300

    Cleanup code, delay example

--- src/WSExampleServer.php
+++ src/WSExampleServer.php
@@ -6,6 +6,7 @@ use Psr\Http\Message\ResponseInterface;
 use Ratchet\ConnectionInterface;
 use Ratchet\RFC6455\Messaging\MessageInterface;
 use Ratchet\WebSocket\MessageComponentInterface;
+use Revolt\EventLoop;
 use function Amp\async;
 use function Amp\delay;
 use function Amp\Future\await;
@@ -25,21 +26,27 @@ class WSExampleServer implements MessageComponentInterface {
 
         for ($i = 0; $i < $options->count; $i++) {
             $time = microtime(true);
-            //$closure = static function () use ($conn, $task, $time, $i) {
-            $closure = fn() => $task->execute()->then(function (ResponseInterface $response) use ($conn, $time, $i) {
-                $res = [
-                    'id' => $i,
-                    'rand' => (string) $response->getBody(),
-                    'time' => (microtime(true) - $time) * 1000,
-                    'memory' => memory_get_usage(),
-                ];
-                $conn->send(json_encode($res));
-            });
+            $closure = static function () use ($conn, $task, $time, $i) {
+                $task->execute()->then(function (ResponseInterface $response) use ($conn, $time, $i) {
+                    $conn->send(json_encode([
+                        'id' => $i,
+                        'rand' => (string) $response->getBody(),
+                        'time' => (microtime(true) - $time) * 1000,
+                        'memory' => memory_get_usage(),
+                    ]));
+                });
+                return null;
+            };
+
             if ($options->async) {
                 // async call with concurrent fibers
-                async($closure);
+                EventLoop::delay(0, $closure);
             } else {
-                // synchronous call
+                //$suspension = EventLoop::getSuspension();
+                //EventLoop::delay(0, static fn() => $suspension->resume());
+                //$suspension->suspend();
+
+                // synchronous series call
                 $closure();
             }
         }
